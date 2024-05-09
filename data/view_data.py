76f1472429c8412ce0ac9f9c77b8718ed80d7f99
dvrk-dvys <ja.harr91@gmail.com>
2024-05-09 20:06:12 +0200

    scrum-15, scrum-10, scrum-19, scrum-20, scrum-13, scrum-22

--- data/view_data.py
+++ data/view_data.py
@@ -1,24 +1,54 @@
 import pickle
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import lit, udf, monotonically_increasing_id
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType, ArrayType, LongType, TimestampType, BinaryType
 
 def read_datafile(path):
     if path.endswith('.pkl'):
-        with open(laptops_test_gold_pkl_file, 'rb') as file:
+        with open(path, 'rb') as file:
             data = pickle.load(file)
-            print(type(data))
-            print(data.keys())
+            print(data)
+            # print(type(data))
+            # print(data.keys())
             print()
+        return data
     elif path.endswith('.parquet'):
         spark = SparkSession.builder.master("local[*]").appName("TiktokComments").getOrCreate()
 
-        parquet_df = (
-            spark.read
-            .parquet(f"{path}", header=True, inferSchema=True)
-            .withColumn("index", monotonically_increasing_id())
+        schema = StructType([
+            StructField("Comment ID", StringType(), True),
+            StructField("Reply to Which Comment", StringType(), True),
+            StructField("User ID", StringType(), True),
+            StructField("Username", StringType(), True),
+            StructField("Nick Name", StringType(), True),
+            StructField("Comment", StringType(), True),
+            StructField("Comment Time", StringType(), True),  # or TimestampType() if you handle datetime objects
+            StructField("Digg Count", IntegerType(), True),
+            StructField("Author Digged", StringType(), True),  # assuming 'Yes' or 'No' can be converted to True/False
+            StructField("Reply Count", IntegerType(), True),
+            StructField("Pinned to Top", StringType(), True),  # assuming 'Yes' or 'No' can be converted to True/False
+            StructField("User Homepage", StringType(), True),
+            StructField("index", LongType(), True),
+            StructField("aspect", StringType(), True),
+            StructField("aspect_mask", ArrayType(LongType(), True), True),
+            StructField("token_ids", ArrayType(LongType(), True), True),
+            StructField("token_type_ids", ArrayType(LongType(), True), True),
+            StructField("attention_mask", ArrayType(LongType(), True), True),
+            StructField("implicitness", BinaryType(), True),  # based on previous error message
+            StructField("polarity", LongType(), True),
+            StructField("raw_text", StringType(), True)
+        ])
+
+        parquet_df = spark.read.schema(schema).parquet(path)
+        print('The df length is: ', parquet_df.count())
+
+        # parquet_df = (
+        #     spark.read
+            # .parquet(f"{path}", header=True, inferSchema=True)
+            # .withColumn("index", monotonically_increasing_id())
             # .limit(self.batch_size)
-        )
-        parquet_df.show(50)
+        # )
+        parquet_df.show(15)
         parquet_rows = parquet_df.collect()
         parquet_array = [list(p) for p in parquet_rows]
         print(parquet_array)
@@ -33,10 +63,16 @@ if __name__ == '__main__':
     old_preprocessed_laptops = '/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/preprocessed/old_laptops_base_google-flan-t5-base.pkl'
 
     out_parquet_path = "/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/gen/train_dataframe.parquet"
+    old_parquet_path = "/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/gen/train_dataframe_old.parquet"
     tt_train = '/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/gen/Tiktok_Train_Implicit_Labeled_preprocess_finetune.pkl'
 
 
-    # read_datafile(out_parquet_path)
-    read_datafile(tt_train)
+    # old_data = read_datafile(old_parquet_path)
+    tt_data = read_datafile(tt_train)
+    laptop_data = read_datafile(laptops_train_gold_pkl_file)
+    # {'aspectTerm': 'mistake'}
 
     print()
+    # [('Comment ID', 'string'), ('Reply to Which Comment', 'string'), ('User ID', 'string'), ('Username', 'string'), ('Nick Name', 'string'), ('Comment', 'string'), ('Comment Time', 'timestamp'), ('Digg Count', 'int'), ('Author Digged', 'string'), ('Reply Count', 'int'), ('Pinned to Top', 'string'), ('User Homepage', 'string'), ('index', 'bigint'), ('aspect', 'string'), ('aspect_mask', 'array<bigint>'), ('token_ids', 'array<bigint>'), ('token_type_ids', 'array<bigint>'), ('attention_mask', 'array<bigint>'), ('implicitness', 'boolean'), ('polarity', 'bigint'), ('raw_text', 'string')]
+    # [('aspect', 'string'), ('aspect_mask', 'array<bigint>'), ('token_ids', 'array<bigint>'), ('token_type_ids', 'array<bigint>'), ('attention_mask', 'array<bigint>'), ('implicitness', 'boolean'), ('polarity', 'bigint'), ('raw_text', 'string')]
+    # [('Comment ID', 'string'), ('Reply to Which Comment', 'string'), ('User ID', 'string'), ('Username', 'string'), ('Nick Name', 'string'), ('Comment', 'string'), ('Comment Time', 'timestamp'), ('Digg Count', 'int'), ('Author Digged', 'string'), ('Reply Count', 'int'), ('Pinned to Top', 'string'), ('User Homepage', 'string'), ('index', 'bigint')]
--- gen_training_data.py
+++ gen_training_data.py
@@ -17,10 +17,13 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import Row
 from pyspark.sql.functions import explode, col, expr, array_join, upper, left, rank
 from pyspark.sql.functions import lit, udf, monotonically_increasing_id
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType, IntegerType, BinaryType, BooleanType
+from pyspark.sql.functions import unix_timestamp, from_unixtime
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType, IntegerType, BinaryType, BooleanType, LongType
 
 from openai import OpenAI
 import json
+from distutils.util import strtobool
+
 
 from src.utils import prompt_direct_inferring, prompt_direct_inferring_masked, prompt_for_aspect_inferring
 
@@ -118,16 +121,37 @@ from src.utils import prompt_direct_inferring, prompt_direct_inferring_masked, p
 # negation then counter info
 # how to rate information density?
 
+def runtime(func):
+        @wraps(func)
+        def runtime_wrapper(*args, **kwargs):
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            total_time = end_time - start_time
+            print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+            return result
+        return runtime_wrapper
 
+def rest_after_run(sleep_seconds=5):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(f"Resting for {sleep_seconds} seconds...")
+            time.sleep(sleep_seconds)
+            print("Starting.")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
-def json_error_handler(max_retries=3, delay_seconds=4, spec=''):
+def json_error_handler(max_retries=3, delay_seconds=8, spec=''):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             for attempt in range(max_retries):
                 try:
                     return func(*args, **kwargs)
-                except (json.JSONDecodeError, IndexError, ValueError) as e:
+                except (json.JSONDecodeError, IndexError, ValueError, AssertionError) as e:
+                    print(f"Error: {type(e).__name__} - {e}")
                     print(f"Error decoding {spec} JSON on attempt {attempt + 1}: {e}")
                     if attempt < max_retries - 1:
                         print(f"Retrying in {delay_seconds} seconds...")
@@ -135,7 +159,8 @@ def json_error_handler(max_retries=3, delay_seconds=4, spec=''):
                     else:
                         print("Max retries exceeded. Run Canceled")
                         break
-                        # return None
+            # return none?
+            # return None
         return wrapper
     return decorator
 
@@ -157,6 +182,55 @@ class genDataset:
                               .appName("TiktokComments")
                               .getOrCreate())
 
+        self.csv_schema = StructType([
+            StructField("Comment ID", StringType(), True),
+            StructField("Reply to Which Comment", StringType(), True),
+            StructField("User ID", StringType(), True),
+            StructField("Username", StringType(), True),
+            StructField("Nick Name", StringType(), True),
+            StructField("Comment", StringType(), True),
+            StructField("Comment Time", StringType(), True),  # or TimestampType() if you handle datetime objects
+            StructField("Digg Count", IntegerType(), True),
+            StructField("Author Digged", StringType(), True),  # assuming 'Yes' or 'No' can be converted to True/False
+            StructField("Reply Count", IntegerType(), True),
+            StructField("Pinned to Top", StringType(), True),  # assuming 'Yes' or 'No' can be converted to True/False
+            StructField("User Homepage", StringType(), True),
+        ])
+
+        self.isa_schema = StructType([
+            StructField("aspect", StringType(), True),
+            StructField("aspect_mask", ArrayType(IntegerType(), True), True),
+            StructField("token_ids", ArrayType(IntegerType(), True), True),
+            StructField("token_type_ids", ArrayType(IntegerType(), True), True),
+            StructField("attention_mask", ArrayType(IntegerType(), True), True),
+            StructField("implicitness", BooleanType(), True),  # based on previous error message
+            StructField("polarity", IntegerType(), True),
+            StructField("raw_text", StringType(), True)
+        ])
+
+        self.final_schema = StructType([
+            StructField("Comment ID", StringType(), True),
+            StructField("Reply to Which Comment", StringType(), True),
+            StructField("User ID", StringType(), True),
+            StructField("Username", StringType(), True),
+            StructField("Nick Name", StringType(), True),
+            StructField("Comment", StringType(), True),
+            StructField("Comment Time", StringType(), True),
+            StructField("Digg Count", IntegerType(), True),
+            StructField("Author Digged", StringType(), True),  # assuming 'Yes' or 'No' can be converted to True/False
+            StructField("Reply Count", IntegerType(), True),
+            StructField("Pinned to Top", StringType(), True),  # assuming 'Yes' or 'No' can be converted to True/False
+            StructField("User Homepage", StringType(), True),
+            StructField("aspect", StringType(), True),
+            StructField("aspect_mask", ArrayType(IntegerType(), True), True),
+            StructField("token_ids", ArrayType(IntegerType(), True), True),
+            StructField("token_type_ids", ArrayType(IntegerType(), True), True),
+            StructField("attention_mask", ArrayType(IntegerType(), True), True),
+            StructField("implicitness", BooleanType(), True),  # based on previous error message
+            StructField("polarity", IntegerType(), True),
+            StructField("raw_text", StringType(), True)
+        ])
+
         self.input_file_path = args.raw_file_path
         self.output_file_path = args.out_file_path
         self.raw_text_col = args.raw_text_col
@@ -175,10 +249,14 @@ class genDataset:
     def intialize_df(self, raw_text_column, out_text_col):
         base_df = (
             self.spark_session.read
+            .schema(self.csv_schema)
             .csv(f"{self.input_file_path}", header=True, inferSchema=True)
             .withColumn("index", monotonically_increasing_id())
             # .limit(self.batch_size)
         )
+        # base_df = base_df.withColumn("Comment Time", col("Comment Time").cast("timestamp"))
+        base_df = base_df.withColumn("Comment Time",
+                                     from_unixtime(unix_timestamp(col("Comment Time"), "dd/MM/yyyy, HH:mm:ss")))
 
         base_df.show(self.batch_size)  # This will print the first 20 rows in the DataFrame.
 
@@ -186,11 +264,12 @@ class genDataset:
         raw_input_array = base_df.select(raw_text_column).rdd.flatMap(lambda x: x).collect()
 
         if os.path.exists(self.output_file_path):
-            self.processed_df = self.spark_session.read.parquet(f"{self.output_file_path}")
+            self.processed_df = self.spark_session.read.schema(self.final_schema).parquet(f"{self.output_file_path}")
+            print('The parquet df length is: ', self.processed_df.count())
             self.processed_df.show()
             self.processed_ids = self.processed_df.select(out_text_col).distinct().rdd.flatMap(lambda x: x).collect()
         else:
-            self.processed_df = self.spark_session.createDataFrame([], schema=base_df.schema)
+            self.processed_df = self.spark_session.createDataFrame([], schema=self.csv_schema)
 
         self.remaining_df = base_df.filter(~base_df[raw_text_column].isin(self.processed_ids))
         self.remaining_df.show()
@@ -269,6 +348,7 @@ class genDataset:
         return flat_list, flat_df
 
     @json_error_handler(max_retries=3, delay_seconds=2, spec='Base GPT Prompt')
+    @rest_after_run(sleep_seconds=2)
     def prompt_gpt(self, role, prompt):
         """
         !!!!!!!THIS IS PAID!!!!!!!
@@ -285,6 +365,7 @@ class genDataset:
         response = json.loads(response)
         print(response)
         assert isinstance(response, list), f"{self.model} output is read to list"
+        assert isinstance(response[0], dict), f"{self.model} output is read to list"
         return response
 
     def generate_aspect_mask(self, sentence_tokens, aspect_tokenized):
@@ -307,6 +388,11 @@ class genDataset:
             self.aspect_masks.append(self.generate_aspect_mask(self.tokens.data['input_ids'][local_index], encoded_aspect_token))
         return self.aspect_masks
 
+    def safe_strtobool(self, value):
+        if isinstance(value, bool):
+            return value
+        return bool(strtobool(str(value)))
+
     def extract_aspects(self, sentence):
         new_context = f'Given the sentence "{sentence}", '
         prompt = new_context + f'which words or phrases are the aspect terms?'
@@ -319,6 +405,7 @@ class genDataset:
         return self.aspects
 
     @json_error_handler(max_retries=3, delay_seconds=2, spec='Aspects')
+    @rest_after_run(sleep_seconds=4)
     def batch_extract_aspects(self, batch_input_array):
         new_context = f'Given these sentences "{batch_input_array}", '
         prompt = new_context + f'which words or phrases are the aspect terms?'
@@ -332,6 +419,7 @@ class genDataset:
             "Ensure the output contains only this JSON array and no additional text."
         )
         self.aspects = self.prompt_gpt(role, prompt)
+        assert len(self.aspects) == len(batch_input_array)
         return self.aspects  #, self.aspect_masks
 
     def extract_polarity_implicitness(self, aspects):
@@ -347,6 +435,7 @@ class genDataset:
         return self.polarity_implicitness
 
     @json_error_handler(max_retries=3, delay_seconds=2, spec='Polarity & Implicits')
+    @rest_after_run(sleep_seconds=4)
     def batch_extract_polarity_implicitness(self, batch_input_array, aspects):
         new_context = f'Given these sentences "{batch_input_array}" and aspect term pairs"{aspects}" which are of length {len(aspects)}, '
         prompt = new_context + f'determine the polairty (positive, negative or neutral) of aspect term and if it is explicitely or implicitely expressed with respect to the whole sentence?'
@@ -360,13 +449,13 @@ class genDataset:
             "Be sure to check for Trailing Commas, Missing/Extra Brackets, Correct Quotation Marks, Special Characters."
             "Ensure the output contains only this JSON array and no additional text.")
         self.polarity_implicitness = self.prompt_gpt(role, prompt)
-        # assert len(self.polarity_implicitness) == len(aspects)
+        assert len(self.polarity_implicitness) == len(aspects)
         return self.polarity_implicitness
 
     def transform_df(self, raw_text, token_ids, token_type_ids, attention_masks, aspect_terms, aspect_mask, polarity_implicitness):
         # aspect_terms = [i['aspectTerm'] for i in aspect]
 
-        implicitness = [i['implicitness'] for i in polarity_implicitness]
+        implicitness = [self.safe_strtobool(i['implicitness']) for i in polarity_implicitness]
         polarity = [i['polarity'] for i in polarity_implicitness]
 
         rows = [
@@ -382,40 +471,61 @@ class genDataset:
             )
             for i in range(len(aspect_terms))
         ]
+        # try:
+        final_train_df = self.spark_session.createDataFrame(rows, self.isa_schema)
+        # except:
+        #     print()
 
-        final_train_df = self.spark_session.createDataFrame(rows)
+        full_final_df = final_train_df.alias('a').join(
+            self.base_df.alias('b'),
+            col('a.' + self.out_text_col) == col('b.' + self.raw_text_col),
+            "left"
+        ).select('b.*', 'a.*')
 
-        print('debug select')
-        final_train_df.show()
-        self.base_df.show()
-        # self.id_text_token_df.show()
-
-        full_final_df = final_train_df.alias('a').join(self.base_df.alias('b'), col('a.' + self.out_text_col) == col('b.' + self.raw_text_col), "left") \
-            .select('b.*',
-                    'a.*')
-        # final_df.show()
+        full_final_df.show()
         return full_final_df
 
+    def write_parquet_file(self, result_df, parquet_path):
+        print('Writing df to Parquet file. See data below.')
+        result_df.show()
+        if not os.path.exists(parquet_path):
+            result_df.write.parquet(parquet_path)
+        else:
+            result_df.write.mode('append').parquet(parquet_path)
 
-    def write_pkl_file(self, result_df, pkl_path):
+    def write_pkl_file(self, pkl_path):
+        result_df = self.spark_session.read.schema(self.final_schema).parquet(f"{self.output_file_path}")
         result_df.printSchema()
-        #
-        train_df = result_df.select(col('aspect').alias('raw_aspect_terms'),
-                                    array_join(col('aspect_mask'), ',').alias('aspect_mask'),
-                                    array_join(col('token_ids'), ',').alias('bert_tokens'),
-                                    array_join(col('token_type_ids'), ',').alias('token_type_ids'),
-                                    array_join(col('attention_mask'), ',').alias('attention_mask'),
-                                    expr("CAST(implicitness AS STRING)").alias('implicits'),
-                                    # expr("CAST(polarity AS STRING)").alias('labels'),
-                                    col('polarity').alias('labels'),
-                                    col('raw_text').alias('raw_texts'))
-        train_df.show()
+        try:
+            train_df = result_df.select(col('raw_text').alias('raw_texts'),
+                                         col('aspect').alias('raw_aspect_terms'),
+                                         col('token_ids').alias('bert_tokens'),
+                                         col('aspect_mask').alias('aspect_masks'),
+                                         col('implicitness').alias('implicits'),
+                                         col('polarity').alias('labels')
+                                        # col('token_type_ids').alias('token_type_ids'),
+                                        # col('attention_mask').alias('attention_mask'),
+            )
+            train_df.show()
 
-        data = train_df.collect()
+            data_rows = train_df.collect()
+            # data = [row.asDict() for row in train_df.collect()]
+            data_dict = {
+                'raw_texts': [row['raw_texts'] for row in data_rows],
+                'raw_aspect_terms': [row['raw_aspect_terms'] for row in data_rows],
+                'bert_tokens': [row['bert_tokens'] for row in data_rows],
+                'aspect_masks': [row['aspect_masks'] for row in data_rows],
+                'implicits': [row['implicits'] for row in data_rows],
+                'labels': [row['labels'] for row in data_rows]
+            }
 
-        with open(pkl_path, 'wb') as file:
-            pickle.dump(data, file)
+            with open(pkl_path, 'wb') as file:
+                pickle.dump(data_dict, file)
+                print("Data successfully written to pickle file.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
+    @runtime
     def run(self):
         # remaining_df = self.input_df
         while self.remaining_df.count() > 0:
@@ -428,15 +538,10 @@ class genDataset:
             self.batch_df = self.prep_token_flatten(self.batch_df, raw_batch_array)
 
             # Bootle Neck
-            # self.aspects, self.base_df = self.flatten_df(self.base_df, self.raw_input_array, self.raw_text_col, self.aspects, 'aspectTerm', dict)
             self.aspects, self.batch_df = self.flatten_df(self.batch_df, raw_batch_array, self.raw_text_col, self.aspects, 'aspectTerm', dict)
-
             # / Bootle Neck
 
-            # self.extract_text_tokens(self.base_df)
-
             self.index = self.batch_df.select('index').rdd.flatMap(lambda x: x).collect()
-            # test = self.base_df.select('index').rdd.flatMap(lambda x: x).collect()
             raw_text = self.batch_df.select(self.raw_text_col).rdd.flatMap(lambda x: x).collect()
             input_ids = self.batch_df.select("input_ids").rdd.flatMap(lambda x: x).collect()
             token_type_ids = self.batch_df.select("token_type_ids").rdd.flatMap(lambda x: x).collect()
@@ -447,14 +552,12 @@ class genDataset:
 
             self.processed_batch_df = self.transform_df(raw_text, input_ids, token_type_ids, attention_mask, self.aspects, self.aspect_masks, self.polarity_implicitness)
             # ------------------------------------------
-
-            self.processed_batch_df.write.mode('append').parquet(self.output_file_path)
-            self.processed_batch_df.show()
+            self.write_parquet_file(self.processed_batch_df, self.output_file_path)
             self.processed_ids = self.processed_batch_df.select(self.raw_text_col).rdd.flatMap(lambda x: x).collect()
             self.remaining_df = self.remaining_df.filter(~self.remaining_df[self.raw_text_col].isin(self.processed_ids))
             self.remaining_df.show()
-            self.write_pkl_file(self.processed_batch_df, self.output_pkl_path)
-            print()
+        # self.write_pkl_file(self.processed_batch_df, self.output_pkl_path)
+        print('Run Complete.')
 
 if __name__ == '__main__':
     raw_file_path = './data/raw/TTCommentExporter-7226101187500723498-201-comments.csv'
@@ -475,25 +578,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     # df = pd.read_csv('/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/raw/TTCommentExporter-7352614724489547051-127-comments.csv')
     gen = genDataset(args=args)
-    result_df = gen.run()
-    result_df.show()
-    result_df.write.parquet("/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/gen/train_dataframe.parquet")
-    # result_df.write.csv("/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/gen/train_dataframe.csv")
-    # result_df.write.json("/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/gen/train_dataframe.json")
-
-    # train_df = result_df.select(col('raw_text').alias('raw_texts'),
-    #                             col('aspect').alias('raw_aspect_terms'),
-    #                             col('token_ids').alias('bert_tokens'),
-    #                             col('aspect_mask').alias('aspect_mask'),
-    #                             col('implicitness').alias('implicits'),
-    #                             col('polarity').alias('labels'))
-    # train_df.show()
-    #
-    # data = train_df.collect()
-    #
-    # with open('/data/gen/Tiktok_Train_Implicit_Labeled_preprocess_finetune.pkl', 'w') as file:
-    #     pickle.dump(data, file)
-
-    # remove the token_ids
-    # result_df.write.csv(path='/Users/jordanharris/Code/PycharmProjects/THOR-ISA-M1/data/gen/Tiktok_Train.csv', mode='overwrite', header=True)
-
+    gen.run()
+    gen.write_pkl_file(out_pkl_path)
