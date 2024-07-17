1ec8e538815255c8b5cb6bc3fadb7d9373847995
dvrk-dvys <ja.harr91@gmail.com>
2024-07-17 13:04:20 +0200

    code for inferring sentiment

--- src/infer_test.py
+++ src/infer_test.py
@@ -35,13 +35,11 @@ class ISA_Infer:
             raise FileNotFoundError(f"Model file not found at {model_path}")
         checkpoint = torch.load(model_path, map_location=self.device)
 
-        # Check if checkpoint contains only the state dict or additional metadata
         if 'model' in checkpoint:
             state_dict = checkpoint['model']
         else:
             state_dict = checkpoint
 
-        # Remove the 'engine.' prefix from the state_dict keys if present
         new_state_dict = {key.replace('engine.', ''): value for key, value in state_dict.items()}
 
         self.model.load_state_dict(new_state_dict)
@@ -55,11 +53,10 @@ class ISA_Infer:
         decoded_output = self.tokenizer.decode(output[0], skip_special_tokens=True)
         print(f"Model Output: {decoded_output}")
 
-        # Extract sentiment and implicitness
-        output_parts = decoded_output.split(', ')
-        sentiment = output_parts[0].split(': ')[1]
-        implicitness = output_parts[1].split(': ')[1]
-        return sentiment, implicitness
+        sentiment = decoded_output.split(', ')
+        #sentiment = output_parts[0].split(': ')[1]
+        #implicitness = output_parts[1].split(': ')[1]
+        return sentiment #, implicitness
 
 
 if __name__ == '__main__':
@@ -70,15 +67,10 @@ if __name__ == '__main__':
     # Print the current working directory
     print("Current Working Directory:", os.getcwd())
 
-    # Create an instance of the inference class
     inference = ISA_Infer(config_path, model_path)
 
-    # Define the input sentence
     input_sentence = "The food was amazing, but the service was terrible."
 
-    # Perform inference
-    sentiment, implicitness = inference.infer(input_sentence)
+    sentiment = inference.infer(input_sentence)
 
-    # Print results
     print(f"Sentiment: {sentiment}")
-    print(f"Implicitness: {implicitness}")
